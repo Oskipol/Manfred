85dec88ec0c9099af7ed85e15812eebce1919d28
Tczek <patik.deda@gmail.com>
2025-09-12 01:44:48 +0200

    -Aded icon after selecting text -Aded feature App now popups and summarize selected area after clicking an icon

--- entrypoints/background.ts
+++ entrypoints/background.ts
@@ -1,10 +1,16 @@
 export default defineBackground(() => {
   chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.type === 'ustaw') {
-      (async()=>{
-        sendResponse(message.tekst+"Kaczka");
+      (async () => {
+        sendResponse(message.tekst);
       })();
     }
+
+    if (message.type === "openPopup") {
+      chrome.action.openPopup();
+      chrome.runtime.sendMessage({ type: "popupAction", action: message.action });
+    }
+
     return true; 
   });
 });
