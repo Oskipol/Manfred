6b04167df32376a405fd46487d5f30187db016ef
Anna Bonikowska <147047809+anbonik@users.noreply.github.com>
2025-09-16 13:45:56 +0200

    Add files via upload

--- simpleChat3.js
+++ simpleChat3.js
@@ -1,19 +1,34 @@
 import { ChatOpenAI } from "@langchain/openai";
+import { ChatPromptTemplate } from "@langchain/core/prompts";
+import fs from "fs/promises";
 
-//model -> nazwa modelu ktory używamy, mozna eksperymentować z roznymi modelami tylko nie z %
+
+const tekst = await fs.readFile("website.txt", "utf8");
+// można podejrzzec czy dobrze nam się strona pobrała
+//console.log(tekst);
+
+//model -> nazwa modelu ktory używamy, mozna eksperymentować z roznymi modelami tylko nie z chat 5
 // openAIApiKey powinno teoretycznie przyjmowac wartosc klucz
 // pozniej mozna jeszcze dodać takie parametry jak temperature(kontrola kreatywności i halucynacjii), 
 // tokens ilosc maksymalnie uzytych tokenów
 const model = new ChatOpenAI({
-    openAIApiKey: "wpisz w te miejsce klucz",
+    
     model: "o4-mini",
 })
 
-//w nawiasie po invoke należy wpisać pytanie jakie chce się zadać chatu
-// mozna tam też wsadzic zmienną typu string z treścia pytania
-// sluzy do tego funkcja prompt templates
-const response = model.invoke('Hello')
-console.log(response)
+//tworzenie schematu prompta w miejsce website zostanie załadowana treśc strony
+const chatPrompt = ChatPromptTemplate.fromMessages([
+  ["system", "You are a helpful assistant that summarizes websites."],
+  [
+    "user",
+    "Summarize the page in less than 700 characters. This should be a continuous text. Highlight the most important information. website: {website}"
+  ]
+]);
 
-//problem jest taki, że wiele wersji ignoruje mozliwosc dodania recznie klucz
-// i wymaga go jako zmiennej srodowiskowej
+//tworzenie prompta. Do schematu prompta daje konkretną tresc strony
+const promptValue = await chatPrompt.invoke({ website: tekst });
+
+//otrzymanie odpowiedzi na prompt
+const response = await model.invoke(promptValue);
+//wystwietlenie odpowiedzi
+console.log(response.content)
